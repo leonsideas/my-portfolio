d08b44da88450aa12cc01d4c9b944f1de882622e
leonsideas <lalbers@hfk-bremen.de>
2026-01-02 14:38:22 +0100

    config

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,8 @@
+
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  // Für Project Pages: https://<user>.github.io/<repo>/
+  // Repo heißt hier "my-portfolio" -> base muss "/my-portfolio/" sein
+  base: '/my-portfolio/',
+})
